afcb286ac350496626c00c738797a8df176cd732
YasminMuntaserN <161508263+YasminMuntaserN@users.noreply.github.com>
2024-09-02 13:40:34 +0300

    feat :add Asynchronous JavaScript AsyncAwait lesson

--- 02-JS-review/script.js
+++ 02-JS-review/script.js
@@ -347,8 +347,24 @@ AfterUpdate;
 
 
 //*****  Asynchronous JavaScript Promises ***********
+/*
 fetch("https://jsonplaceholder.typicode.com/todos")
   .then((res) => res.json())
   .then((data) => console.log(data));
 
 console.log("jonas");
+*/
+
+//*****  Asynchronous JavaScript AsyncAwait ***********
+/*
+async function getTodos(){
+  const res =await fetch("https://jsonplaceholder.typicode.com/todos");
+  const data = await res.json();
+  console.log(data);
+  return data;
+}
+
+const todos =getTodos();
+console.log(todos);
+console.log("yasmin");
+*/

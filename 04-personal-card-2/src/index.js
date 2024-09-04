5ba0e1316c45db760da982340b9cca7e2e3bf30a
YasminMuntaserN <161508263+YasminMuntaserN@users.noreply.github.com>
2024-09-04 18:14:19 +0300

    refactor : edit SkillList function

--- 04-personal-card-2/src/index.js
+++ 04-personal-card-2/src/index.js
@@ -42,9 +42,6 @@ function App() {
       <Avatar />
       <div className="data">
         <Intro />
-        {/* Should contain one Skill component
-        for each web dev skill that you have,
-        customized with props */}
         <SkillList />
       </div>
     </div>

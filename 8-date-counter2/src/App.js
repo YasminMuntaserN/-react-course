c6142cf2622d66b7ae87a77db6d4aa13d7ea4c19
YasminMuntaserN <161508263+YasminMuntaserN@users.noreply.github.com>
2024-09-07 06:34:50 +0300

    refactor:edit the style file

--- 8-date-counter2/src/App.js
+++ 8-date-counter2/src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+
+
+function Counter(){
+  const [step , setStep]= useState(1);
+  const [count , setcount]= useState(step === 1 ? 0: step);
+
+  // Calculate the new date based on the current count
+  const newDate = new Date();;
+  newDate.setDate(newDate.getDate() + count);
+
+  return (
+    <div>
+      <input type="range" min ="0" max="10" />
+      <div className="steps">
+      <button className ="sub" onClick={()=>setStep(c => c-1)}> - </button>
+      <p> Step: {step}</p>
+      <button className ="add" onClick={()=>setStep(c => c+1)}> + </button>
+      </div>
+
+      <div className="counts">
+      <button className ="sub" onClick={()=>setcount(c => c-step)} > - </button>
+      <p> Count: {count}</p>
+      <button className ="add" onClick={()=>setcount(c => c+step)} > + </button>
+      </div>
+
+      <div>
+          <p className='dayesNum'>
+              {count == 0 ? `Today is `:
+              count > 0 ? `${count} dayes from Today is ` :
+               `${count * -1} dayes ago was `}
+              {newDate.toDateString() }
+          </p>
+      </div>
+      </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Counter/>
     </div>
   );
 }
